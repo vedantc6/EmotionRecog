4ebccf8d1424e01a11568748407932674aa7077d
vedantc6 <vedantc6@gmail.com>
2017-04-27 17:02:11 +0530

    Model

--- emotion.py
+++ emotion.py
@@ -1,24 +1,19 @@
 import numpy as np
 from load_data import load_data
 
-from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation, Flatten
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
-from keras.utils import np_utils
-
 np.random.seed(1337)
 
 # TRAIN A CONVNET
 
-batch_size = 128
-epochs = 16
-classes = 7
+nb_batch_size = 128
+nb_epochs = 16
+nb_classes = 7
 
 # Input image dimensions
 img_rows, img_col = 48, 48
 # Number of convolutional filters
-filters = 32
+nb_filters = 32
 # Size of pooling area
-pool = 2
+nb_pool = 2
 # convolutional kernel size
-conv = 3
+nb_conv = 3
---
+++ model.py
@@ -0,0 +1,33 @@
+from keras.models import Sequential
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.optimizers import Adadelta
+
+# Input image dimensions
+img_rows, img_cols = 48, 48
+# Number of convolutional filters
+nb_filters = 32
+# Size of pooling area
+nb_pool = 2
+# convolutional kernel size
+nb_conv = 3
+nb_classes = 7
+
+model = Sequential()
+
+model.add(Convolution2D(nb_filters, nb_conv, nb_conv, border_mode='full', input_shape=(1, img_rows, img_cols)))
+model.add(Activation('relu'))
+model.add(Convolution2D(nb_filters, nb_conv, nb_conv))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dropout(0.25))
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer=Adadelta(), metrics=['accuracy'])
+model.save('FerModel.h5')
